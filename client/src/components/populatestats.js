9cf341a60fc24a15b97314bf6fed27030ad0f48c
bencornell4 <bencornell44@gmail.com>
2024-01-24 14:47:35 -0800

    Profile Menu
    Added default prof pic and hover to profile menu

--- client/src/components/populatestats.js
+++ client/src/components/populatestats.js
@@ -12,28 +12,40 @@ function getStatsElement(gameName) {
 
 let sameEvent = false;
 
-document.getElementById("postContainer").addEventListener('mouseover', (event) => {
+document.addEventListener('mouseover', (event) => {
     if (sameEvent) {
         return;
     }
-    if (event.target.name == "populate-stats-hover") {  
-        //display stats block
+    if (event.target.name == "populate-stats-hover-feed") { 
         var element = getHoveredImage(event);
-        var rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
         var username = element.parentElement.parentElement.nextElementSibling.firstElementChild.getElementsByTagName("h3")[0].textContent;
-        statsDisplay.style.left = (element.offsetLeft + element.offsetWidth) / rootFontSize + 1 + "rem";
-        statsDisplay.style.top = element.offsetTop / rootFontSize + "rem";
-        statsDisplay.style.display = "inline-block";
-
-        //populate stats
-        nameDisplay.textContent = username;
-        var gameStats = getStatsElement("fortnite");
-        fortniteRequests(username, gameStats);
-        sameEvent = true;
+        populateStatsBlock(element, username, 0);
+    } else if (event.target.name == "populate-stats-hover-user") {  
+        var element = getHoveredImage(event);
+        populateStatsBlock(element, reqUser(), 2.5 * element.offsetWidth);
     }
 });
 
-document.getElementById("postContainer").addEventListener('mouseout', (event) => {
+function populateStatsBlock(element, username, rightOffset) {
+    //display stats block
+    var rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    //statsDisplay.style.left = (element.offsetLeft + element.offsetWidth) / rootFontSize + 1 + "rem";
+    //statsDisplay.style.top = element.offsetTop / rootFontSize + "rem";
+    statsDisplay.style.top = (element.offsetTop + element.offsetHeight) / rootFontSize + 1 + "rem";
+    statsDisplay.style.left = (element.offsetLeft - rightOffset) / rootFontSize + "rem";
+    statsDisplay.style.display = "inline-block";
+    nameDisplay.textContent = username;
+    var gameStats = getStatsElement("fortnite");
+    fortniteRequests(username, gameStats);
+    sameEvent = true;
+}
+
+function reqUser() {
+    //temporary for testing
+    return "bencornell44";
+}
+
+document.addEventListener('mouseout', (event) => {
     if (sameEvent) {
         var gameStats = getStatsElement("fortnite");
         nameDisplay.textContent = "Loading...";
